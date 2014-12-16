687b53ae9c24d08d8cbc2b4bfe79aa46e09cd035
toddskinner <todd.skinner@gmail.com>
2014-12-15 18:42:08 -0800

    Add blinkyDancer execept last test

--- src/dancer.js
+++ src/dancer.js
@@ -1,7 +1,45 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
 
-  var dancer = {};
+  //var dancer = {};
+
+  // use jQuery to create an HTML <span> tag
+  this.$node = $('<span class="dancer"></span>');
+
+
+  this.step();
+
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+
+//  return dancer;
+};//makeDancer
+
+  makeDancer.prototype.step = function(){
+    // the basic dancer doesn't do anything interesting at all on each step,
+    // it just schedules the next step
+    var that = this;
+    setTimeout(this.step, this.timeBetweenSteps);
+  };
+
+  makeDancer.prototype.setPosition = function(top, left){
+    // Use css top and left properties to position our <span> tag
+    // where it belongs on the page. See http://api.jquery.com/css/
+    //
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    this.$node.css(styleSettings);
+  };
+/****** OLD CODE *********
+
+// Creates and returns a new dancer object that can step
+var makeDancer = function(top, left, timeBetweenSteps){
+
+  //var dancer = {};
 
   // use jQuery to create an HTML <span> tag
   dancer.$node = $('<span class="dancer"></span>');
@@ -31,3 +69,9 @@ var makeDancer = function(top, left, timeBetweenSteps){
 
   return dancer;
 };
+
+
+
+
+
+ */
