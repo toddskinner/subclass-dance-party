8ab579f41d75044d99c707ee9c857aa034d0186c
toddskinner <todd.skinner@gmail.com>
2014-12-15 19:52:38 -0800

    Add blinkyDancer with all tests

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-  makeDancer.call(this, top, left, timeBetweenSteps);
+  makeDancer.call( this,top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
@@ -20,7 +20,9 @@ makeBlinkyDancer.prototype.step = function(){
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
+
   this.$node.toggle();
+
 };//blinkyDancer
 
 
--- src/dancer.js
+++ src/dancer.js
@@ -4,9 +4,9 @@ var makeDancer = function(top, left, timeBetweenSteps){
   //var dancer = {};
 
   // use jQuery to create an HTML <span> tag
+
   this.$node = $('<span class="dancer"></span>');
-
-
+  this.time_steps = timeBetweenSteps;
   this.step();
 
 
@@ -20,9 +20,15 @@ var makeDancer = function(top, left, timeBetweenSteps){
   makeDancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    var that = this;
-    setTimeout(this.step, this.timeBetweenSteps);
-  };
+    //var that = this;
+    //
+    // var that = this;   this this refers to makeDancer obj
+    // var stepTime = function(){
+    //   that.step();
+    // };
+
+    setTimeout( this.step.bind(this), this.time_steps);  //the bound this refers to the caller of step which will be a makeBlinkyDancer obj
+  };// the first this (i.e. context) gets replaced by the second this
 
   makeDancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
